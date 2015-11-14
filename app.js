1b19f26e4cdc8b87c9e70efc64772819d73711fe
Kirk Graves <graves.kirk@gmail.com>
2015-11-14 11:12:27 -0600

    registered firebase as dependency

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('MusicHistoryApp', ['ngRoute']);
+var app = angular.module('MusicHistoryApp', ['ngRoute', 'firebase']);
 
 app.config(['$routeProvider',
   function($routeProvider) {
